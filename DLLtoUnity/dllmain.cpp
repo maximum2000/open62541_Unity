e6383fa40ddb34f132ae71fdede5ffd6dcc46288
max gammer <gammermax@gmail.com>
2023-10-31 22:28:02 +0500

    +

--- DLLtoUnity/dllmain.cpp
+++ DLLtoUnity/dllmain.cpp
@@ -1,10 +1,20 @@
-﻿//M:\GIT2\open62541_Unity\open62541 - 1.3.8\examples\tutorial_client_firststeps.c
+﻿#include <stdlib.h>
+#include <string>
 
+
+//server
 #include <open62541/client_config_default.h>
 #include <open62541/client_highlevel.h>
 #include <open62541/plugin/log_stdout.h>
-#include <stdlib.h>
-#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+
+//client
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+#include <signal.h>
+
 
 
 
@@ -43,13 +53,204 @@ void SendLog(const std::wstring& str, const int& color)
 //SendLog(L"debug DLL:connectionLost", 0);
 
 
+//-----------------------------------------------------------------------------------------
+
+
+
+static void addVariable(UA_Server* server)
+{
+    /* Define the attribute of the myInteger variable node */
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    UA_Int32 myInteger = 42;
+    UA_Variant_setScalar(&attr.value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+    attr.description = UA_LOCALIZEDTEXT((char*)"en - US", (char*)"the answer");
+    attr.displayName = UA_LOCALIZEDTEXT((char*)"en-US", (char*)"the answer");
+    attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+
+    /* Add the variable node to the information model */
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, (char*)"the.answer");
+    UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, (char*)"the answer");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_Server_addVariableNode(server, myIntegerNodeId, parentNodeId,
+        parentReferenceNodeId, myIntegerName,
+        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), attr, NULL, NULL);
+}
+
+
+static void addMatrixVariable(UA_Server* server) 
+{
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    attr.displayName = UA_LOCALIZEDTEXT((char*)"en-US", (char*)(char*)"Double Matrix");
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+
+    /* Set the variable value constraints */
+    attr.dataType = UA_TYPES[UA_TYPES_DOUBLE].typeId;
+    attr.valueRank = UA_VALUERANK_TWO_DIMENSIONS;
+    UA_UInt32 arrayDims[2] = { 2,2 };
+    attr.arrayDimensions = arrayDims;
+    attr.arrayDimensionsSize = 2;
+
+    /* Set the value. The array dimensions need to be the same for the value. */
+    UA_Double zero[4] = { 0.0, 0.0, 0.0, 0.0 };
+    UA_Variant_setArray(&attr.value, zero, 4, &UA_TYPES[UA_TYPES_DOUBLE]);
+    attr.value.arrayDimensions = arrayDims;
+    attr.value.arrayDimensionsSize = 2;
+
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, (char*)"double.matrix");
+    UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, (char*)"double matrix");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_Server_addVariableNode(server, myIntegerNodeId, parentNodeId,
+        parentReferenceNodeId, myIntegerName,
+        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE),
+        attr, NULL, NULL);
+}
+
+/**
+ * Now we change the value with the write service. This uses the same service
+ * implementation that can also be reached over the network by an OPC UA client.
+ */
+
+static void writeVariable(UA_Server* server) {
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, (char*)"the.answer");
+
+    /* Write a different integer value */
+    UA_Int32 myInteger = 43;
+    UA_Variant myVar;
+    UA_Variant_init(&myVar);
+    UA_Variant_setScalar(&myVar, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+    UA_Server_writeValue(server, myIntegerNodeId, myVar);
+
+    /* Set the status code of the value to an error code. The function
+     * UA_Server_write provides access to the raw service. The above
+     * UA_Server_writeValue is syntactic sugar for writing a specific node
+     * attribute with the write service. */
+    UA_WriteValue wv;
+    UA_WriteValue_init(&wv);
+    wv.nodeId = myIntegerNodeId;
+    wv.attributeId = UA_ATTRIBUTEID_VALUE;
+    wv.value.status = UA_STATUSCODE_BADNOTCONNECTED;
+    wv.value.hasStatus = true;
+    UA_Server_write(server, &wv);
+
+    /* Reset the variable to a good statuscode with a value */
+    wv.value.hasStatus = false;
+    wv.value.value = myVar;
+    wv.value.hasValue = true;
+    UA_Server_write(server, &wv);
+}
+
+/**
+ * Note how we initially set the DataType attribute of the variable node to the
+ * NodeId of the Int32 data type. This forbids writing values that are not an
+ * Int32. The following code shows how this consistency check is performed for
+ * every write.
+ */
+
+static void writeWrongVariable(UA_Server* server) 
+{
+    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, (char*)"the.answer");
+
+    /* Write a string */
+    UA_String myString = UA_STRING((char*)"test");
+    UA_Variant myVar;
+    UA_Variant_init(&myVar);
+    UA_Variant_setScalar(&myVar, &myString, &UA_TYPES[UA_TYPES_STRING]);
+    UA_StatusCode retval = UA_Server_writeValue(server, myIntegerNodeId, myVar);
+    printf("Writing a string returned statuscode %s\n", UA_StatusCode_name(retval));
+}
+
+/** It follows the main server code, making use of the above definitions. */
+
+static volatile UA_Boolean running = true;
+//static void stopHandler(int sign) {
+//    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+//    running = false;
+//}
+
+extern "C" __declspec(dllexport) int testServer(int a, int b)
+{
+   // signal(SIGINT, stopHandler);
+   // signal(SIGTERM, stopHandler);
+    
+   SendLog(L"debug DLL:testServer", 0);
+
+    UA_Server* server = UA_Server_new();
+    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+    UA_ServerConfig* config = UA_Server_getConfig(server);
+    config->verifyRequestTimestamp = UA_RULEHANDLING_ACCEPT;
+    #ifdef UA_ENABLE_WEBSOCKET_SERVER
+        UA_ServerConfig_addNetworkLayerWS(UA_Server_getConfig(server), 7681, 0, 0, NULL, NULL);
+    #endif
+
+    SendLog(L"debug DLL:addVariable", 0);
+    addVariable(server);
+    addMatrixVariable(server);
+    writeVariable(server);
+    writeWrongVariable(server);
+
+                    //UA_StatusCode retval = UA_Server_run(server, &running);
+                    //UA_Server_delete(server);
+                    //return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+
+    /* Should the server networklayer block (with a timeout) until a message
+       arrives or should it return immediately? */
+    UA_Boolean waitInternal = false;
+
+
+    SendLog(L"debug DLL:UA_Server_run_startup", 0);
+    UA_StatusCode retval = UA_Server_run_startup(server);
+    if (retval != UA_STATUSCODE_GOOD)
+        goto cleanup;
+
+    while (running) {
+        /* timeout is the maximum possible delay (in millisec) until the next
+           _iterate call. Otherwise, the server might miss an internal timeout
+           or cannot react to messages with the promised responsiveness. */
+           /* If multicast discovery server is enabled, the timeout does not not consider new input data (requests) on the mDNS socket.
+            * It will be handled on the next call, which may be too late for requesting clients.
+            * if needed, the select with timeout on the multicast socket server->mdnsSocket (see example in mdnsd library)
+            */
+        UA_UInt16 timeout = UA_Server_run_iterate(server, waitInternal);
+
+        /* Now we can use the max timeout to do something else. In this case, we
+           just sleep. (select is used as a platform-independent sleep
+           function.) */
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = timeout * 1000;
+        select(0, NULL, NULL, NULL, &tv);
+    }
+    retval = UA_Server_run_shutdown(server);
+
+cleanup:
+    UA_Server_delete(server);
+    return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------
 
 //nothrow
-extern "C" __declspec(dllexport) int bopen(int a, int b)
+extern "C" __declspec(dllexport) int testClient(int a, int b)
 {
     UA_Client* client = UA_Client_new();
     UA_ClientConfig_setDefault(UA_Client_getConfig(client));
@@ -81,6 +282,30 @@ extern "C" __declspec(dllexport) int bopen(int a, int b)
     UA_Client_delete(client); // Disconnects the client internally 
     return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -149,73 +374,6 @@ return 0;
 }
 */
 
-
-
-
-/*
-
-
-#include <stdlib.h>
-
-int main(void) {
-    UA_Client* client = UA_Client_new();
-    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
-    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
-    if (retval != UA_STATUSCODE_GOOD) {
-        UA_Client_delete(client);
-        return (int)retval;
-    }
-
-    // Read the value attribute of the node. UA_Client_readValueAttribute is a
-    // wrapper for the raw read service available as UA_Client_Service_read. 
-    UA_Variant value; // Variants can hold scalar values and arrays of any type 
-    UA_Variant_init(&value);
-
-    // NodeId of the variable holding the current time 
-    const UA_NodeId nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
-    retval = UA_Client_readValueAttribute(client, nodeId, &value);
-
-    if (retval == UA_STATUSCODE_GOOD &&
-        UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
-        UA_DateTime raw_date = *(UA_DateTime*)value.data;
-        UA_DateTimeStruct dts = UA_DateTime_toStruct(raw_date);
-        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "date is: %u-%u-%u %u:%u:%u.%03u\n",
-            dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
-    }
-
-    // Clean up 
-    UA_Variant_clear(&value);
-    UA_Client_delete(client); // Disconnects the client internally 
-    return EXIT_SUCCESS;
-}
-
-*/
-
-/**
- * Compilation is similar to the server example.
- *
- * .. code-block:: bash
- *
- *     $ gcc -std=c99 open62541.c myClient.c -o myClient
- *
- * In a MinGW environment, the Winsock library must be added.
- *
- * .. code-block:: bash
- *
- *    $ gcc -std=c99 open62541.c myClient.c -lws2_32 -o myClient.exe
- *
- * Further tasks
- * ^^^^^^^^^^^^^
- *
- * - Try to connect to some other OPC UA server by changing
- *   ``opc.tcp://localhost:4840`` to an appropriate address (remember that the
- *   queried node is contained in any OPC UA server).
- *
- * - Try to set the value of the variable node (ns=1,i="the.answer") containing
- *   an ``Int32`` from the example server (which is built in
- *   :doc:`tutorial_server_firststeps`) using "UA_Client_write" function. The
- *   example server needs some more modifications, i.e., changing request types.
- *   The answer can be found in ``examples/client.c``. */
 
 
 
---
+++ UnityTest/Assets/OPC_UA/OPCUA_SERVER_DLL.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AOT; // MonopInvokeCallback
+using System.Runtime.InteropServices;
+using System;
+using System.Text;
+using TMPro;
+using UnityEngine.Events;
+
+using System.Threading;
+
+
+[System.Serializable]
+public class MyInteractionEvent : UnityEvent<int> { }
+
+
+public class OPCUA_SERVER_DLL : MonoBehaviour
+{
+    /// Background thread for TcpServer workload.  
+    private Thread My_Thread;
+    private Mutex mutexObj;
+    bool stop = false;
+
+
+    public MyInteractionEvent ReceiveInteractionEvent;
+    public static List<int> allEvent;
+    //public  Action<int> HealthChangedAction;
+
+
+   //имя класса для создаваемого объекта
+   public TMP_InputField IP;
+
+   
+
+
+    //Получить хендл класса по имени (ambassador->getObjectClassHandle(L"HLAobjectRoot.ObjectClass0"))
+    [DllImport("DLL1")]
+    public static extern int testServer(int a, int b);
+    //public static extern int GetObjectClassHandle(StringBuilder className, int length);
+
+    
+
+    //callback для Debug'а
+    private delegate void DebugCallback(IntPtr message, int color, int size);
+    [DllImport("DLL1", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void RegisterDebugCallback(DebugCallback callback);
+    //вывод сообщения из debug'а DLL'ки
+    private static void DebugLog(IntPtr message, int color, int size)
+    {
+        string debugString = Marshal.PtrToStringAnsi(message, size);
+        Debug.Log("c# debug:" + debugString);
+    }
+    //end callback для Debug'а
+
+    
+    //-----------------------------------------------------------------------------------------------
+
+    public void RegisterCallback()
+    {
+        //регистрация всех callbeck функций (привязка)
+        {
+            RegisterDebugCallback(DebugLog);
+           
+        }
+
+
+        
+    }
+
+   
+
+    public void startServer()
+    {
+        //StringBuilder str2 = new StringBuilder(FedarationName.text, 100);
+        //string filename = Application.streamingAssetsPath + "/fdd_test.xml";
+        //StringBuilder str3 = new StringBuilder(filename, 100);
+        //CreateFederationExecution(str2, str2.Capacity, str3);
+        //string myString2 = str2.ToString();
+        //Debug.Log("c# debug:" + "CreateFederationExecution: " + myString2);
+
+        int a = 1;
+        int b = 1;
+        testServer(a, b);
+
+
+    }
+
+
+
+
+
+
+
+void OnDestroy()
+    {
+        RegisterDebugCallback(null);
+        
+    }
+
+
+
+
+
+    void Start()
+    {
+    }
+
+        // Start is called before the first frame update
+    public void StartThread()
+    {
+        allEvent = new List<int>();
+
+        mutexObj = new Mutex();
+        My_Thread = new Thread(new ThreadStart(Thread_loop));
+        My_Thread.IsBackground = true;
+        My_Thread.Start();
+
+        // mutexObj.Dispose();
+        // My_Thread.Abort();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        return;
+
+        GetReadedBytes();
+
+        //List<bool> allEvent
+        for (int i=0; i < allEvent.Count; i++)
+        {
+            int z = allEvent[i];
+            if (ReceiveInteractionEvent != null)
+            {
+                ReceiveInteractionEvent.Invoke(z);
+            }
+        }
+            
+    }
+
+
+    private void Thread_loop()
+    {
+        while (stop==false)
+        {
+            //evokeCallback(0.1);
+            //getObjectClassHandle();
+        }
+    }
+
+    public List<int> GetReadedBytes()
+    {
+        if (stop == true)
+        {
+            return new List<int>();
+        }
+
+        mutexObj.WaitOne();
+        List<int> ReadedBytesDeepCopy = new List<int>();
+        foreach (byte b in allEvent)
+        {
+            ReadedBytesDeepCopy.Add(b);
+        }
+        allEvent.Clear();
+        mutexObj.ReleaseMutex();
+        return ReadedBytesDeepCopy;
+    }
+
+}
+
+
