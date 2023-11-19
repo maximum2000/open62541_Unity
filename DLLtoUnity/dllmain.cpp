9cde80d7553435c45348af7197c0be6134b798f7
max gammer <gammermax@gmail.com>
2023-11-19 22:00:15 +0500

    +

--- DLLtoUnity/dllmain.cpp
+++ DLLtoUnity/dllmain.cpp
@@ -8,6 +8,8 @@ copyright            : (C) 2023 by Гаммер Максим Дмитриеви�
 email                : Maxim.Gammer@yandex.ru
 ***************************************************************************/
 
+//#define UA_ENABLE_DISCOVERY_MULTICAST true
+
 #include <stdlib.h>
 #include <string>
 #include <sstream>
@@ -1225,81 +1227,215 @@ extern "C" __declspec(dllexport) void OPC_ClientSubscriptionAddVariable(char* _o
 
 //10. OPC_Client_findServers()
 #define DISCOVERY_SERVER_ENDPOINT "opc.tcp://localhost:4840"
+
+void Logg(UA_String uaString)
+{
+    char* convert = (char*)UA_malloc(sizeof(char) * uaString.length + 1);
+    memcpy(convert, uaString.data, uaString.length);
+    convert[uaString.length] = '\0';
+
+    std::wstringstream cls2;
+    cls2 << convert;
+    std::wstring svalue = cls2.str();
+
+    SendLog(svalue, 0);
+}
+void Logg2(std::string wString)
+{
+    std::wstringstream cls2;
+    cls2 << wString.c_str();
+    std::wstring svalue = cls2.str();
+
+    SendLog(svalue, 0);
+}
+
 extern "C" __declspec(dllexport) int OPC_Client_findServers()
 {
-//    #ifdef UA_ENABLE_DISCOVERY
-    UA_ServerOnNetwork* serverOnNetwork = NULL;
-    size_t serverOnNetworkSize = 0;
-
-    UA_Client* client = UA_Client_new();
-    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
-    UA_StatusCode retval = UA_Client_findServersOnNetwork(client, DISCOVERY_SERVER_ENDPOINT, 0, 0, 0, NULL, &serverOnNetworkSize, &serverOnNetwork);
-    if (retval != UA_STATUSCODE_GOOD) 
+    //Server representing a local discovery server as a central instance.
+    //Any other server can register with this server(see server_register.c).Clients can then call the find servers service to get all registered servers(see client_find_servers.c).
+    /*
     {
-       // UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
-       //     "Could not call FindServersOnNetwork service. "
-       //     "Is the discovery server started? StatusCode %s",
-       //     UA_StatusCode_name(retval));
+        UA_Boolean running = true;
+        UA_Server* server = UA_Server_new();
+        UA_ServerConfig* config = UA_Server_getConfig(server);
+        UA_ServerConfig_setDefault(config);
 
-        std::wstringstream cls2;
-        cls2 << L"Could not call FindServersOnNetwork service. Is the discovery server started? StatusCode";
-        std::wstring svalue = cls2.str();
+        // This is an LDS server only. Set the application type to DISCOVERYSERVER.
+        // NOTE: This will cause UaExpert to not show this instance in the server list.
+        // See also: https://forum.unified-automation.com/topic1987.html
+        config->applicationDescription.applicationType = UA_APPLICATIONTYPE_DISCOVERYSERVER;
+        UA_String_clear(&config->applicationDescription.applicationUri);
+        config->applicationDescription.applicationUri = UA_String_fromChars("urn:open62541.example.local_discovery_server");
 
-        SendLog(svalue, 0);
+        // Enable the mDNS announce and response functionality
+        config->mdnsEnabled = true;
 
-        UA_Client_delete(client);
-        return EXIT_SUCCESS;
+        config->mdnsConfig.mdnsServerName = UA_String_fromChars("LDS");
+
+        // See http://www.opcfoundation.org/UA/schemas/1.03/ServerCapabilities.csv
+        // For a LDS server, you should only indicate the LDS capability.
+        // If this instance is an LDS and at the same time a normal OPC UA server, you also have to indicate
+        // the additional capabilities.
+        // NOTE: UaExpert does not show LDS-only servers in the list.
+        // See also: https://forum.unified-automation.com/topic1987.html
+
+        // E.g. here we only set LDS, and you will not see it in UaExpert
+        config->mdnsConfig.serverCapabilitiesSize = 1;
+        UA_String* caps = (UA_String*)UA_Array_new(1, &UA_TYPES[UA_TYPES_STRING]);
+        caps[0] = UA_String_fromChars("LDS");
+        config->mdnsConfig.serverCapabilities = caps;
+
+        // timeout in seconds when to automatically remove a registered server from
+        // the list, if it doesn't re-register within the given time frame. A value
+        // of 0 disables automatic removal. Default is 60 Minutes (60*60). Must be
+        //bigger than 10 seconds, because cleanup is only triggered approximately
+        // every 10 seconds. The server will still be removed depending on the
+        // state of the semaphore file. 
+         // config->discoveryCleanupTimeout = 60*60;
+
+        UA_StatusCode retval = UA_Server_run(server, &running);
+
+        //UA_Server_delete(server);
+        return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+    }
+    */
+
+
+    //Example for calling FindServersOnNetwork
+    {
+        //    #ifdef UA_ENABLE_DISCOVERY
+        UA_ServerOnNetwork* serverOnNetwork = NULL;
+        size_t serverOnNetworkSize = 0;
+        bool ok = true;
+        UA_Client* client = UA_Client_new();
+        UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+        UA_StatusCode retval = UA_Client_findServersOnNetwork(client, DISCOVERY_SERVER_ENDPOINT, 0, 0, 0, NULL, &serverOnNetworkSize, &serverOnNetwork);
+        if (retval != UA_STATUSCODE_GOOD)
+        {
+            // UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,
+            //     "Could not call FindServersOnNetwork service. "
+            //     "Is the discovery server started? StatusCode %s",
+            //     UA_StatusCode_name(retval));
+            Logg2("Could not call FindServersOnNetwork service. Is the discovery server started? StatusCode");
+            UA_Client_delete(client);
+            ok = false;
+            //return EXIT_SUCCESS;
+        }
+
+        if (ok == true)
+        {
+            // output all the returned/registered servers
+            for (size_t i = 0; i < serverOnNetworkSize; i++)
+            {
+                UA_ServerOnNetwork* server = &serverOnNetwork[i];
+
+                {
+                    //printf("Server[%lu]: %.*s", (unsigned long)i, (int)server->serverName.length, server->serverName.data);
+                    //UA_String uaString = *(UA_String*)server->serverName.data;
+                    //Logg(uaString);
+                    std::string temp1((char*)server->serverName.data, (int)server->serverName.length);
+                    Logg2(temp1);
+                }
+
+                //printf("\n\tRecordID: %u", server->recordId);
+                {
+                    //printf("\n\tDiscovery URL: %.*s", (int)server->discoveryUrl.length, server->discoveryUrl.data);
+                    //UA_String uaString = *(UA_String*)server->discoveryUrl.data;
+                    //Logg(uaString);
+                    std::string temp1((char*)server->discoveryUrl.data, (int)server->discoveryUrl.length);
+                    Logg2(temp1);
+                }
+                //printf("\n\tCapabilities: ");
+                //for (size_t j = 0; j < server->serverCapabilitiesSize; j++) 
+                //{
+                //    printf("%.*s,", (int)server->serverCapabilities[j].length, server->serverCapabilities[j].data);
+                //}
+                //printf("\n\n");
+            }
+            UA_Array_delete(serverOnNetwork, serverOnNetworkSize, &UA_TYPES[UA_TYPES_SERVERONNETWORK]);
+        }
     }
 
-    // output all the returned/registered servers
-    for (size_t i = 0; i < serverOnNetworkSize; i++) 
+    
+    //Example for calling FindServers
     {
-        UA_ServerOnNetwork* server = &serverOnNetwork[i];
         
+        bool ok = true;
+
+        UA_ApplicationDescription* applicationDescriptionArray = NULL;
+        size_t applicationDescriptionArraySize = 0;
+
+        UA_StatusCode retval;
         {
-            //printf("Server[%lu]: %.*s", (unsigned long)i, (int)server->serverName.length, server->serverName.data);
-            UA_String uaString = *(UA_String*)server->serverName.data;
-
-            char* convert = (char*)UA_malloc(sizeof(char) * uaString.length + 1);
-            memcpy(convert, uaString.data, uaString.length);
-            convert[uaString.length] = '\0';
-
-            std::wstringstream cls2;
-            cls2 << convert;
-            std::wstring svalue = cls2.str();
-
-            SendLog(svalue, 0);
-
+            UA_Client* client = UA_Client_new();
+            UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+            retval = UA_Client_findServers(client, DISCOVERY_SERVER_ENDPOINT, 0, NULL, 0, NULL, &applicationDescriptionArraySize, &applicationDescriptionArray);
+            UA_Client_delete(client);
+        }
+        if (retval != UA_STATUSCODE_GOOD) 
+        {
+            //UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Could not call FindServers service. Is the discovery server started? StatusCode %s", UA_StatusCode_name(retval));
+            Logg2("Could not call FindServers service. Is the discovery server started?");
+            ok = false;
+            //return EXIT_FAILURE;
         }
 
+        SendLog(L"OKOK", 0);
 
-        //printf("\n\tRecordID: %u", server->recordId);
+        
+        if (ok == true)
         {
-            //printf("\n\tDiscovery URL: %.*s", (int)server->discoveryUrl.length, server->discoveryUrl.data);
-            UA_String uaString = *(UA_String*)server->discoveryUrl.data;
+            // output all the returned/registered servers
+            for (size_t i = 0; i < applicationDescriptionArraySize; i++)
+            {
+                SendLog(L"*", 0);
+                UA_ApplicationDescription* description = &applicationDescriptionArray[i];
+                //printf("Server[%lu]: %.*s", (unsigned long)i, (int)description->applicationUri.length,  description->applicationUri.data);
+                //printf("\n\tName: %.*s", (int)description->applicationName.text.length,  description->applicationName.text.data);
+                //printf("\n\tApplication URI: %.*s", (int)description->applicationUri.length, description->applicationUri.data);
+                //printf("\n\tProduct URI: %.*s", (int)description->productUri.length, description->productUri.data);
+                //printf("\n\tType: ");
 
-            char* convert = (char*)UA_malloc(sizeof(char) * uaString.length + 1);
-            memcpy(convert, uaString.data, uaString.length);
-            convert[uaString.length] = '\0';
+                std::string temp1((char*)description->applicationName.text.data, (int)description->applicationName.text.length);
+                Logg2(temp1);
 
-            std::wstringstream cls2;
-            cls2 << convert;
-            std::wstring svalue = cls2.str();
-
-            SendLog(svalue, 0);
-
+                switch (description->applicationType) 
+                {
+                case UA_APPLICATIONTYPE_SERVER:
+                    Logg2("Server");
+                    //printf("Server");
+                    break;
+                case UA_APPLICATIONTYPE_CLIENT:
+                    Logg2("Client");
+                    //printf("Client");
+                    break;
+                case UA_APPLICATIONTYPE_CLIENTANDSERVER:
+                    Logg2("Client and Server");
+                    //printf("Client and Server");
+                    break;
+                case UA_APPLICATIONTYPE_DISCOVERYSERVER:
+                    Logg2("Discovery Server");
+                    //printf("Discovery Server");
+                    break;
+                default:
+                    Logg2("Unknown");
+                    //printf("Unknown");
+                }
+                //printf("\n\tDiscovery URLs:");
+                
+                for (size_t j = 0; j < description->discoveryUrlsSize; j++) 
+                {
+                    printf("\n\t\t[%lu]: %.*s", (unsigned long)j,  (int)description->discoveryUrls[j].length, description->discoveryUrls[j].data);
+                    UA_String uaString = *(UA_String*)description->discoveryUrls[j].data;
+                    std::string z((char*)description->discoveryUrls[j].data, (int)description->discoveryUrls[j].length);
+                    Logg2(z);
+                }
+                //printf("\n\n");
+                
+            }
         }
-
-
-        //printf("\n\tCapabilities: ");
-        //for (size_t j = 0; j < server->serverCapabilitiesSize; j++) 
-        //{
-        //    printf("%.*s,", (int)server->serverCapabilities[j].length, server->serverCapabilities[j].data);
-        //}
-        //printf("\n\n");
+        
     }
-
-    UA_Array_delete(serverOnNetwork, serverOnNetworkSize, &UA_TYPES[UA_TYPES_SERVERONNETWORK]);
 
 
 //#endif
